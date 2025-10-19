2b284f0966fa08769de62cce262574931971a771
Cham-Myae-Thwin <6731503050@lamduan.mfu.ac.th>
2025-10-19 13:28:05 +0000

    change name

--- src/main/java/th/mfu/boot/User.java
+++ src/main/java/th/mfu/boot/User.java
@@ -3,19 +3,20 @@ package th.mfu.boot;
 public class User {
 
     private String username;
-    private String displayName;
+    private String displayname;
     private String email;
+    
     public String getUsername() {
         return username;
     }
     public void setUsername(String username) {
         this.username = username;
     }
-    public String getDisplayName() {
-        return displayName;
+    public String getDisplayname() {
+        return displayname;
     }
-    public void setDisplayName(String displayName) {
-        this.displayName = displayName;
+    public void setDisplayname(String displayname) {
+        this.displayname = displayname;
     }
     public String getEmail() {
         return email;
@@ -23,6 +24,5 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
-
-    
 }
+    
